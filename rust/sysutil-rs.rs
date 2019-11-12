91f0555a32708474432dbc7cb9820559be350415
navdeepkk <navdeep.navdeep37@gmail.com>
2019-11-12 23:48:55 +0530

    added upto line 208

--- rust/sysutil-rs.rs
+++ rust/sysutil-rs.rs
@@ -153,7 +153,8 @@ unsafe extern "C" fn vsf_sysutil_install_sighandler(sig: EVSFSysUtilSignal,
 unsafe extern "C" fn vsf_sysutil_default_sig(sig: EVSFSysUtilSignal)
 {
   let mut realsig: u32 = vsf_sysutil_translate_sig(sig);
-  SIG_DFL =  { Some( std::mem::transmute::<isize,unsafe extern "C" fn (arg1 : :: std :: os :: raw :: c_int)>(-1))};
+  //SIG_DFL has value 0 from signum-generic.h
+  SIG_DFL =  { Some( std::mem::transmute::<isize,unsafe extern "C" fn (arg1 : :: std :: os :: raw :: c_int)>(0))};
   match SIG_DFL{
     None=>{},
     Some(x)=>{
@@ -161,6 +162,47 @@ unsafe extern "C" fn vsf_sysutil_default_sig(sig: EVSFSysUtilSignal)
     }
   }
   s_sig_details[realsig as usize].p_private = ptr::null_mut();
+  s_sig_details[realsig as usize].sync_sig_handler = Some( std::mem::transmute::<isize,
+    unsafe extern "C" fn (arg1 :*mut :: std :: os :: raw :: c_void)>(0) );
+}
 
-  s_sig_details[realsig as usize].sync_sig_handler = Some( std::mem::transmute::<isize,unsafe extern "C" fn (arg1 :*mut :: std :: os :: raw :: c_void)>(0) );;
+unsafe extern "C" fn vsf_sysutil_install_null_sighandler(sig: EVSFSysUtilSignal)
+{
+  let mut realsig: u32 = vsf_sysutil_translate_sig(sig);
+  //SIG_DFL has value 0 from signum-generic.h
+  SIG_DFL =  { Some( std::mem::transmute::<isize,unsafe extern "C" fn (arg1 : :: std :: os :: raw :: c_int)>(0))};
+  match SIG_DFL{
+    None=>{},
+    Some(x)=>{
+        vsf_sysutil_set_sighandler(realsig, x);
+    }
+  }
+  s_sig_details[realsig as usize].p_private = ptr::null_mut();
+  s_sig_details[realsig as usize].sync_sig_handler = Some( std::mem::transmute::<isize,
+    unsafe extern "C" fn (arg1 :*mut :: std :: os :: raw :: c_void)>(0) );
+  vsf_sysutil_set_sighandler(realsig, vsf_sysutil_common_sighandler);
+}
+
+unsafe extern "C" fn vsf_sysutil_install_async_sighandler( sig: EVSFSysUtilSignal, handler: vsf_async_sighandle_t)
+{
+    let mut realsig: u32 = vsf_sysutil_translate_sig(sig);
+    //SIG_DFL has value 0 from signum-generic.h
+    SIG_DFL =  { Some( std::mem::transmute::<isize,unsafe extern "C" fn (arg1 : :: std :: os :: raw :: c_int)>(0))};
+    match SIG_DFL{
+      None=>{},
+      Some(x)=>{
+          vsf_sysutil_set_sighandler(realsig, x);
+      }
+    }
+  s_sig_details[realsig as usize].p_private = ptr::null_mut();
+  s_sig_details[realsig as usize].sync_sig_handler = Some( std::mem::transmute::<isize,
+    unsafe extern "C" fn (arg1 :*mut :: std :: os :: raw :: c_void)>(0) );
+  vsf_sysutil_block_sig(sig);
+  match handler{
+      None=>{},
+      Some(x)=>{
+        vsf_sysutil_set_sighandler(realsig, x);
+      }
+  }
+  
 }
